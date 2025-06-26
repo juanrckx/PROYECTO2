cd1fb6194eb7b90dc664459df8b125cfa7ef74e0
juanrckx <jjuam2006@gmail.com>
2025-06-25 20:07:27 -0600

    PROYECTO-2

---
+++ src/camera.py
@@ -0,0 +1,10 @@
+import pygame
+
+
+class Camera:
+    def __init__(self, width, height):
+        self.camera = pygame.Rect(0, 0, width, height)
+        self.width = width
+        self.height = height
+
+    def apply (self.entity):
